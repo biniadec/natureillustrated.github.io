818c220b2ef2c220d7b329f2af7b417aeecfcc57
biniadec <biniadec@gmail.com>
2026-06-26 13:24:53 +0200

    Make mobile version lool good

---
+++ site-config.js
@@ -0,0 +1,14 @@
+window.NATURE_ILLUSTRATED_CONFIG = {
+  contact: {
+    email: 'nature.illustrated@gmail.com',
+  },
+  shop: {
+    url: 'https://natureillustrated.myspreadshop.dk/',
+  },
+  social: {
+    // Add profile URLs to show these optional links in the contact section.
+    instagram: '',
+    linkedin: '',
+    bluesky: '',
+  },
+};
